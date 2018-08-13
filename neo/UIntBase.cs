aeac2a81546d74e255bef0b53d578e6ef1645c93
HurryGuo <guopengpeng@neo.org>
2018-08-13 15:49:42 +0800

    Add tracing log to UIntBase.cs

--- neo/UIntBase.cs
+++ neo/UIntBase.cs
@@ -2,6 +2,7 @@
 using System;
 using System.IO;
 using System.Linq;
+using DbgViewTR;
 
 namespace Neo
 {
@@ -13,64 +14,76 @@ namespace Neo
 
         protected UIntBase(int bytes, byte[] value)
         {
+            TR.Enter();
             if (value == null)
             {
                 this.data_bytes = new byte[bytes];
+                TR.Exit();
                 return;
             }
             if (value.Length != bytes)
                 throw new ArgumentException();
             this.data_bytes = value;
+            TR.Exit();
         }
 
         void ISerializable.Deserialize(BinaryReader reader)
         {
+            TR.Enter();
             reader.Read(data_bytes, 0, data_bytes.Length);
+            TR.Exit();
         }
 
         public bool Equals(UIntBase other)
         {
+            TR.Enter();
             if (ReferenceEquals(other, null))
-                return false;
+                return TR.Exit(false);
             if (ReferenceEquals(this, other))
-                return true;
+                return TR.Exit(true);
             if (data_bytes.Length != other.data_bytes.Length)
-                return false;
-            return data_bytes.SequenceEqual(other.data_bytes);
+                return TR.Exit(false);
+            return TR.Exit(data_bytes.SequenceEqual(other.data_bytes));
         }
 
         public override bool Equals(object obj)
         {
+            TR.Enter();
             if (ReferenceEquals(obj, null))
-                return false;
+                return TR.Exit(false);
             if (!(obj is UIntBase))
-                return false;
-            return this.Equals((UIntBase)obj);
+                return TR.Exit(false);
+            return TR.Exit(this.Equals((UIntBase)obj));
         }
 
         public override int GetHashCode()
         {
-            return data_bytes.ToInt32(0);
+            TR.Enter();
+            return TR.Exit(data_bytes.ToInt32(0));
         }
 
         public static UIntBase Parse(string s)
         {
+            TR.Enter();
             if (s.Length == 40 || s.Length == 42)
-                return UInt160.Parse(s);
+                return TR.Exit(UInt160.Parse(s));
             else if (s.Length == 64 || s.Length == 66)
-                return UInt256.Parse(s);
+                return TR.Exit(UInt256.Parse(s));
             else
                 throw new FormatException();
         }
 
         void ISerializable.Serialize(BinaryWriter writer)
         {
+            TR.Enter();
             writer.Write(data_bytes);
+            TR.Exit();
         }
 
         public byte[] ToArray()
         {
-            return data_bytes;
+            TR.Enter();
+            return TR.Exit(data_bytes);
         }
 
         /// <summary>
@@ -79,11 +92,13 @@ namespace Neo
         /// <returns>返回16进制字符串</returns>
         public override string ToString()
         {
-            return "0x" + data_bytes.Reverse().ToHexString();
+            TR.Enter();
+            return TR.Exit("0x" + data_bytes.Reverse().ToHexString());
         }
 
         public static bool TryParse<T>(string s, out T result) where T : UIntBase
         {
+            TR.Enter();
             int size;
             if (typeof(T) == typeof(UInt160))
                 size = 20;
@@ -100,7 +115,7 @@ namespace Neo
                 if (UInt160.TryParse(s, out UInt160 r))
                 {
                     result = (T)(UIntBase)r;
-                    return true;
+                    return TR.Exit(true);
                 }
             }
             else if (size == 32)
@@ -108,25 +123,27 @@ namespace Neo
                 if (UInt256.TryParse(s, out UInt256 r))
                 {
                     result = (T)(UIntBase)r;
-                    return true;
+                    return TR.Exit(true);
                 }
             }
             result = null;
-            return false;
+            return TR.Exit(false);
         }
 
         public static bool operator ==(UIntBase left, UIntBase right)
         {
+            TR.Enter();
             if (ReferenceEquals(left, right))
-                return true;
+                return TR.Exit(true);
             if (ReferenceEquals(left, null) || ReferenceEquals(right, null))
-                return false;
-            return left.Equals(right);
+                return TR.Exit(false);
+            return TR.Exit(left.Equals(right));
         }
 
         public static bool operator !=(UIntBase left, UIntBase right)
         {
-            return !(left == right);
+            TR.Enter();
+            return TR.Exit(!(left == right));
         }
     }
 }
