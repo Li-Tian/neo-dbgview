d6dadb341d5b3bcca150ec0de5ac9295d7152ec4
HurryGuo <guopengpeng@neo.org>
2018-08-15 11:42:31 +0800

    Add tracing log to StateDescriptor.cs

--- neo/Core/StateDescriptor.cs
+++ neo/Core/StateDescriptor.cs
@@ -6,6 +6,7 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using DbgViewTR;
 
 namespace Neo.Core
 {
@@ -34,18 +35,23 @@ namespace Neo.Core
 
         private void CheckAccountState()
         {
+            TR.Enter();
             if (Key.Length != 20) throw new FormatException();
             if (Field != "Votes") throw new FormatException();
+            TR.Exit();
         }
 
         private void CheckValidatorState()
         {
+            TR.Enter();
             if (Key.Length != 33) throw new FormatException();
             if (Field != "Registered") throw new FormatException();
+            TR.Exit();
         }
 
         void ISerializable.Deserialize(BinaryReader reader)
         {
+            TR.Enter();
             Type = (StateType)reader.ReadByte();
             if (!Enum.IsDefined(typeof(StateType), Type))
                 throw new FormatException();
@@ -61,17 +67,19 @@ namespace Neo.Core
                     CheckValidatorState();
                     break;
             }
+            TR.Exit();
         }
 
         private Fixed8 GetSystemFee_Validator()
         {
+            TR.Enter();
             switch (Field)
             {
                 case "Registered":
                     if (Value.Any(p => p != 0))
-                        return Fixed8.FromDecimal(1000);
+                        return TR.Exit(Fixed8.FromDecimal(1000));
                     else
-                        return Fixed8.Zero;
+                        return TR.Exit(Fixed8.Zero);
                 default:
                     throw new InvalidOperationException();
             }
@@ -79,41 +87,46 @@ namespace Neo.Core
 
         void ISerializable.Serialize(BinaryWriter writer)
         {
+            TR.Enter();
             writer.Write((byte)Type);
             writer.WriteVarBytes(Key);
             writer.WriteVarString(Field);
             writer.WriteVarBytes(Value);
+            TR.Exit();
         }
 
         public JObject ToJson()
         {
+            TR.Enter();
             JObject json = new JObject();
             json["type"] = Type;
             json["key"] = Key.ToHexString();
             json["field"] = Field;
             json["value"] = Value.ToHexString();
-            return json;
+            return TR.Exit(json);
         }
 
         internal bool Verify()
         {
+            TR.Enter();
             switch (Type)
             {
                 case StateType.Account:
-                    return VerifyAccountState();
+                    return TR.Exit(VerifyAccountState());
                 case StateType.Validator:
-                    return VerifyValidatorState();
+                    return TR.Exit(VerifyValidatorState());
                 default:
-                    return false;
+                    return TR.Exit(false);
             }
         }
 
         private bool VerifyAccountState()
         {
+            TR.Enter();
             switch (Field)
             {
                 case "Votes":
-                    if (Blockchain.Default == null) return false;
+                    if (Blockchain.Default == null) return TR.Exit(false);
                     ECPoint[] pubkeys;
                     try
                     {
@@ -121,34 +134,35 @@ namespace Neo.Core
                     }
                     catch (FormatException)
                     {
-                        return false;
+                        return TR.Exit(false);
                     }
                     UInt160 hash = new UInt160(Key);
                     AccountState account = Blockchain.Default.GetAccountState(hash);
-                    if (account?.IsFrozen != false) return false;
+                    if (account?.IsFrozen != false) return TR.Exit(false);
                     if (pubkeys.Length > 0)
                     {
-                        if (account.GetBalance(Blockchain.GoverningToken.Hash).Equals(Fixed8.Zero)) return false;
+                        if (account.GetBalance(Blockchain.GoverningToken.Hash).Equals(Fixed8.Zero)) return TR.Exit(false);
                         HashSet<ECPoint> sv = new HashSet<ECPoint>(Blockchain.StandbyValidators);
                         DataCache<ECPoint, ValidatorState> validators = Blockchain.Default.GetStates<ECPoint, ValidatorState>();
                         foreach (ECPoint pubkey in pubkeys)
                             if (!sv.Contains(pubkey) && validators.TryGet(pubkey)?.Registered != true)
-                                return false;
+                                return TR.Exit(false);
                     }
-                    return true;
+                    return TR.Exit(true);
                 default:
-                    return false;
+                    return TR.Exit(false);
             }
         }
 
         private bool VerifyValidatorState()
         {
+            TR.Enter();
             switch (Field)
             {
                 case "Registered":
-                    return true;
+                    return TR.Exit(true);
                 default:
-                    return false;
+                    return TR.Exit(false);
             }
         }
     }
