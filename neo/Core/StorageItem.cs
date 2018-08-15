5d8950c212e94abe888a7cb40ba919bdad0edc78
HurryGuo <guopengpeng@neo.org>
2018-08-15 11:59:39 +0800

    Add tracing log to StorageItem.cs

--- neo/Core/StorageItem.cs
+++ neo/Core/StorageItem.cs
@@ -1,5 +1,6 @@
 ﻿using Neo.IO;
 using System.IO;
+using DbgViewTR;
 
 namespace Neo.Core
 {
@@ -11,27 +12,34 @@ namespace Neo.Core
 
         StorageItem ICloneable<StorageItem>.Clone()
         {
-            return new StorageItem
+            TR.Enter();
+            return TR.Exit(new StorageItem
             {
                 Value = Value
-            };
+            });
         }
 
         public override void Deserialize(BinaryReader reader)
         {
+            TR.Enter();
             base.Deserialize(reader);
             Value = reader.ReadVarBytes();
+            TR.Exit();
         }
 
         void ICloneable<StorageItem>.FromReplica(StorageItem replica)
         {
+            TR.Enter();
             Value = replica.Value;
+            TR.Exit();
         }
 
         public override void Serialize(BinaryWriter writer)
         {
+            TR.Enter();
             base.Serialize(writer);
             writer.WriteVarBytes(Value);
+            TR.Exit();
         }
     }
 }
